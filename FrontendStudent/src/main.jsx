ca1391a28208c0a4fcec24f012c2ae4aaa443ab6
deepaksingh1227 <divyanthakur856@gmail.com>
2026-01-09 13:34:24 +0530

    fix Assignment generation

--- FrontendStudent/src/main.jsx
+++ FrontendStudent/src/main.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <React.StrictMode>
+    <QueryClientProvider client={new QueryClient()}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </QueryClientProvider>
+  </React.StrictMode>
 );
